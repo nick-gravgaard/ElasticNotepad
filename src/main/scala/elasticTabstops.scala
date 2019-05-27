16c7129bb8608550a44eb2136cec162bd713c853
Nick Gravgaard <nick@nickgravgaard.com>
2019-05-27 18:49:51 +0100

    Refactor code and transpose array rather than list

--- src/main/scala/elasticTabstops.scala
+++ src/main/scala/elasticTabstops.scala
@@ -28,9 +28,9 @@ package object elasticTabstops {
   // Replace each item in a run with its highest value.
   // scala>        maxAdjacent(List(Some(1), Some(2), None, Some(4), None, None, Some(7), Some(8), Some(9)))
   // res0: List[Option[Int]] = List(Some(2), Some(2), None, Some(4), None, None, Some(9), Some(9), Some(9))
-  private def maxAdjacent(list: List[Option[Int]]): List[Option[Int]] = {
+  private def maxAdjacent(column: List[Option[Int]]): List[Option[Int]] = {
     def fillMax = (l: List[Option[Int]]) => List.fill(l.length)(Some(l.flatten.max))
-    processAdjacent(fillMax, list)
+    processAdjacent(fillMax, column)
   }
 
   private def calcMaxedWidthsPerLine(textWidthsPerLine: List[List[Int]]) : List[List[Int]] = {
@@ -68,13 +68,22 @@ package object elasticTabstops {
   // Replace each item in a run with None if all of its contents are empty strings.
   // scala>      replaceEmptyRuns(List(Some(""), Some("a"), None, Some(""), Some("")))
   // res0: List[Option[String]] = List(Some(""), Some("a"), None, None,     None))
-  private def replaceEmptyRuns(list: List[Option[String]]): List[Option[String]] = {
+  private def replaceEmptyRuns(column: Array[Option[String]]): Array[Option[String]] = {
     def nonesIfAllEmpty = (l: List[Option[String]]) =>
       if (l.forall(_.contains(""))) List.fill[Option[String]](l.length)(None) else l
-    processAdjacent(nonesIfAllEmpty, list)
+    processAdjacent(nonesIfAllEmpty, column.toList).toArray
   }
 
-  def spacesToTabs(text: String): String = {
+  private def transpose(a: Array[Array[Option[String]]]): Array[Array[Option[String]]] = {
+    val nofRows = a.length
+    // we know that all rows have the same number of columns
+    val nofCols = a.headOption.map(_.length).getOrElse(0)
+    val b = Array.ofDim[Option[String]](nofCols, nofRows)
+    for (y <- 0 until nofRows; x <- 0 until nofCols) b(x)(y) = a(y)(x)
+    b
+  }
+
+  private def getPossCellsFromText(text: String): Array[Array[Option[String]]] = {
     // a non-space followed by any number of chars that are either a non-space or a space followed by a non-space
     val cellTextRegEx = "[^ ](?:[^ ]| (?=[^ ]))*".r
 
@@ -84,21 +93,29 @@ package object elasticTabstops {
     // get maps for each line containing the position of text and the text itself
     val matchesPerLine = prefixedTextPerLine.map(cellTextRegEx.findAllMatchIn(_).map(m => m.start -> m.matched).toMap)
 
+    // make a sorted set of possible cell positions using the flattened list of positions as varargs
     val allPositions = SortedSet(matchesPerLine.map(_.keys).toList.flatten: _*)
 
     // for each line, create matched or empty strings at every possible cell position
-    val possCellsPerLine = matchesPerLine.map(matchesThisLine =>
+    val matchesAndEmptiesPerLine = matchesPerLine.map(matchesThisLine =>
       allPositions.toArray.map(position =>
         if (matchesThisLine.contains(position)) Some(matchesThisLine(position)) else Some("")))
 
     // we know that empty strings at the end of the line cannot be cells, so replace them with None
-    val possCellsPerLine2 = possCellsPerLine.map { possCellsThisLine =>
+    matchesAndEmptiesPerLine.map { possCellsThisLine =>
       val nofTrailingEmpties = possCellsThisLine.reverse.takeWhile(_.contains("")).length
       possCellsThisLine.take(possCellsThisLine.length - nofTrailingEmpties) ++ List.fill(nofTrailingEmpties)(None)
     }
+  }
 
-    // transpose, replace empty columns with Nones, transpose back, remove Nones, and join with tabs
-    val textPerLine = possCellsPerLine2.toList.transpose.map(replaceEmptyRuns).transpose.map(_.flatten).map(_.mkString("\t"))
+  def spacesToTabs(text: String): String = {
+    val possCellsPerLine = getPossCellsFromText(text)
+
+    // transpose so we can iterate over columns
+    val possCellsPerCol = transpose(possCellsPerLine)
+
+    // replace empty columns with Nones, transpose back, remove Nones, and join with tabs
+    val textPerLine = possCellsPerCol.toList.map(replaceEmptyRuns).transpose.map(_.flatten).map(_.mkString("\t"))
 
     // finally, drop previously inserted non-whitespace character from each line and join with newlines
     textPerLine.map(_.drop(1)).mkString("\n")
